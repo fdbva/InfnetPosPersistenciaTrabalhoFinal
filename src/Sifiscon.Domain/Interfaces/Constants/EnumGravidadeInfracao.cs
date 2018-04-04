713a20c4f33be6482ce9424c5a3bcc74884c225b
fdbva <villela.felipe@gmail.com>
2018-04-04 20:59:43 -0300

    Add Constants Enum

---
+++ src/Sifiscon.Domain/Interfaces/Constants/EnumGravidadeInfracao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sifiscon.Domain.Interfaces.Constants
+{
+    public enum EnumGravidadeInfracao
+    {
+        Um = 1,
+        Dois,
+        Três
+    }
+}
