5f8ecf590151429f9203c3e41bc5db470250b05e
fdbva <villela.felipe@gmail.com>
2018-04-04 20:56:40 -0300

    Added UnitOfWork

---
+++ src/Sifiscon.Infra.Data/UoW/UnitOfWork.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sifiscon.Infra.Data.Context;
+using Sifiscon.Infra.Data.Interfaces;
+
+namespace Sifiscon.Infra.Data.UoW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly SifisconContext _context;
+        private bool _disposed;
+
+        public UnitOfWork(SifisconContext context)
+        {
+            _context = context;
+        }
+
+        public void BeginTransaction()
+        {
+            _disposed = false;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            _disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
