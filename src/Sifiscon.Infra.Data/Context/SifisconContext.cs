d77a80841a836dc7bb164223788739a18095a55a
fdbva <villela.felipe@gmail.com>
2018-04-04 21:00:22 -0300

    Added Context

---
+++ src/Sifiscon.Infra.Data/Context/SifisconContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Sifiscon.Domain.Entities;
+
+namespace Sifiscon.Infra.Data.Context
+{
+    public class SifisconContext : DbContext
+    {
+        public SifisconContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+        public DbSet<AutoDeInfracao> AutosDeInfracao { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+        public DbSet<Processo> Processos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
