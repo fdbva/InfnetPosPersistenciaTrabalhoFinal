cd6c0a1bf5fa8a06560ddb2d784047804c84cda1
fdbva <villela.felipe@gmail.com>
2018-04-04 22:04:14 -0300

    ProcessoRepository constructor was missing

--- src/Sifiscon.Infra.Data/Repositories/ProcessoRepository.cs
+++ src/Sifiscon.Infra.Data/Repositories/ProcessoRepository.cs
@@ -1,10 +1,13 @@
 ﻿using Sifiscon.Domain.Entities;
 using Sifiscon.Domain.Interfaces.Repositories;
+using Sifiscon.Infra.Data.Context;
 
 namespace Sifiscon.Infra.Data.Repositories
 {
     public class ProcessoRepository : RepositoryBase<Processo>, IProcessoRepository
     {
-        
+        public ProcessoRepository(SifisconContext context) : base(context)
+        {
+        }
     }
 }
